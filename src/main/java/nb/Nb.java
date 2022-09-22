46e578930829e83da952c8f1d753aaca350a410e
Gilles <chekunive@gmail.com>
2022-09-22 19:23:00 +0200

    init

--- src/main/java/nb/Nb.java
+++ src/main/java/nb/Nb.java
@@ -10,17 +10,47 @@ import static java.util.stream.IntStream.range;
 public class Nb {
     int n = 8;
     List<Paire> ens0 = new ArrayList<>();
-    Supplier<IntStream> I = () -> range(1, n+1);
+    Supplier<IntStream> I = () -> range(1, n + 1);
+    List<Paire>[][] tens = new ArrayList[n + 1][n + 1];
+
+    {
+        for (int i = 1; i < n + 1; i++)
+            for (int j = 1; j < n + 1; j++)
+                tens[i][j] = new ArrayList<>();
+    }
+
 
     public Nb() {
 
-        I.get().forEach(k -> I.get().forEach(l ->
+//        I.get().forEach(k -> I.get().forEach(l ->
+//                {
+//                    if (k <= l) ens0.add(new Paire(k, l));
+//                }
+//        ));
+//        for(Paire p:ens0)
+//            System.out.print(p+";");
+
+        I.get().forEach(j -> I.get().forEach(i ->
+                I.get().forEach(k -> I.get().forEach(l ->
+                        {
+                            if (k != i && k != j && l != i && l != j) tens[i][j].add(new Paire(k, l));
+                        }
+                ))
+        ));
+        I.get().forEach(j -> I.get().forEach(i ->
+
                 {
-                    if (k <= l) ens0.add(new Paire(k, l));
+                    System.out.println("("+i+","+j+")");
+                    for (Paire t : tens[i][j]) System.out.print(t + ";");
+                    System.out.print('\n');
                 }
         ));
-        for(Paire p:ens0)
-            System.out.print(p+";");
+
+    }
+
+    public static void main(String[] args) {
+        new Nb();
+
     }
 
     private static class Paire {
@@ -35,10 +65,5 @@ public class Nb {
         public String toString() {
             return "(" + k + "," + l + ")";
         }
-    }
-
-    public static void main(String[] args) {
-        new Nb();
-
     }
 }
